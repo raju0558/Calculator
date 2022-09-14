30463e5e40be0e434c10f515eddaa04f3c93bf05
jntuk-ccc <jntuk-ccc@DESKTOP-48BJBNU>
2022-09-14 12:47:39 +0530

    commit4

--- src/main/java/ETA_SB/jntu_calc_application/Calculator.java
+++ src/main/java/ETA_SB/jntu_calc_application/Calculator.java
@@ -5,22 +5,20 @@ public class Calculator {
 		return num1 + num2;
 	}
 
+	
+	
+	
+	
+	
 	public double doSub(double num1, double num2) {
 		//comment line
 		return num1 - num2;
 	}
 
 	
-	
 	public double doMul(double num1, double num2) {
 		return num1 * num2;
 	}
-
-	
-	
-	
-	
-	
 	
 	public double doDiv(double num1, double num2) {
 		return num1 / num2;
